a22680aacb11f10aed620c244db80331dffe47bd
mppub <michal.podhradsky@gmail.com>
2021-06-08 11:31:27 +0200

    progress

--- README.md
+++ README.md
@@ -11,18 +11,10 @@ Prerequisites:
   Tasks:
   - Log to console current non 0 asset balances available on the SPOT account (testnet)
   - Open a single userData websocket (with all the requirement logic to keep the listenKey active)
-  wss://stream.binance.com:9443
-  
   - Keep your local asset balances state up to date based on the data coming from userData
   - Log the asset balances again on every balance change
-
-
-
   - Open 10 *@trade websockets for the 10 pairs with the highest volume in the last 24h on the SPOT exchange (mainnet)
   - Determinate the 10 pairs dynamically (no hard-coded pairs)
-
-
   - Measure event time => client receive time latency and log (min/mean/max) to console every 1 minute
-  - https://binance-docs.github.io/apidocs/spot/en/#test-connectivity
 ```
 
--- index.js
+++ index.js
@@ -2,44 +2,10 @@ const WebSocket = require('ws');
 const https = require('https');
 const { createHmac } = require('crypto');
 const { setEnvVariablesSync, handleExceptionsAndRejections } = require('./utils.js')
-/*
 
-Prerequisites:
-  - Runs on node.js (latest LTS version)
-  - Maximum of 2 external libraries allowed (less the better) (sub-dependencies do not count here)
-  - No binance/crypto related libraries allowed
-  - SPOT testnet available at https://testnet.binance.vision
-  - SPOT mainnet available at https://www.binance.com
-  - SPOT documentation available at https://binance-docs.github.io/apidocs/spot/en/#change-log
-
-  Tasks:
-  - Log to console current non 0 asset balances available on the SPOT account (testnet)
-  - Open a single userData websocket (with all the requirement logic to keep the listenKey active)
-  wss://stream.binance.com:9443
-
-  - Keep your local asset balances state up to date based on the data coming from userData
-  - Log the asset balances again on every balance change
-
-
-
-  - Open 10 *@trade websockets for the 10 pairs with the highest volume in the last 24h on the SPOT exchange (mainnet)
-    - https://binance-docs.github.io/apidocs/spot/en/#current-average-price
-    - https://binance-docs.github.io/apidocs/spot/en/#24hr-ticker-price-change-statistics
-    - or https://binance-docs.github.io/apidocs/spot/en/#individual-symbol-mini-ticker-stream if v the same
-  - Determinate the 10 pairs dynamically (no hard-coded pairs)
-
-
-  - Measure event time => client receive time latency and log (min/mean/max) to console every 1 minute
-    - https://binance-docs.github.io/apidocs/spot/en/#test-connectivity
- */
 
 setEnvVariablesSync();
 handleExceptionsAndRejections()
-
-/*
-mainnet: wss://stream.binance.com:9443
- */
-
 
 const config = {
   testnet: {
@@ -154,30 +120,62 @@ const createWSClient = (address, state) => {
   console.log(address)
   const ws = new WebSocket(address);
 
-  ws.on('open', function open(ev) {
-    console.log('connection open', ev)
-    // ws.send('something');
+  ws.on('open', () => {
+
   });
 
-  ws.on('message', function incoming(data) {
-    console.log('incoming data:')
+  ws.on('message', (data) => {
+    console.log('incoming data:', data)
+    console.log(`typeof data`, typeof data);
 
-    /* TODO:
-    update state prices according to those events that should arrive:
-    - https://binance-docs.github.io/apidocs/spot/en/#payload-account-update
-    - https://binance-docs.github.io/apidocs/spot/en/#payload-balance-update
-     */
-    console.log(data);
+    let parsedData
+    try {
+      parsedData = JSON.parse(data)
+    }
+    catch (e) {
+      console.error('WSClient: error while parsing message: ', data)
+      throw e;
+    }
+
+    switch (parsedData.e) {
+      case 'outboundAccountPosition':
+        parsedData['B'].forEach((el) => {
+          if (Number(el.free) === 0 || Number(el.locked) === 0) {
+            delete state.spot.nonZeroBalances[`${el.a}`];
+          } else {
+            state.spot.nonZeroBalances[`${el.a}`] = { free: el.f, locked: el.l }
+          }
+        })
+
+        console.log(`Current non-zero spot account balances: `, state.spot.nonZeroBalances)
+        break;
+      /*
+      note: can not test this on testnet and not 100% sure, that the "d"(Balance Delta) param actually mean the asset change and can have negative values, I would just do:
+
+      case 'balanceUpdate':
+        const currentBalanceObj = state.spot.nonZeroBalances[`${parsedData.a}`]
+        const currentAmount = Number(currentBalanceObj.free) - Number(parsedData['d'])
+        if (currentAmount <= 0 && Number(currentBalanceObj.locked) === 0) {
+          delete state.spot.nonZeroBalances[`${parsedData.a}`];
+        } else {
+          state.spot.nonZeroBalances[`${parsedData.a}`] = {
+            free: `${currentAmount}`,
+            locked: currentBalanceObj.locked
+          }
+        }
+
+        console.log(`Current non-zero spot account balances: `, state.spot.nonZeroBalances)
+        break;
+       */
+    }
   });
 
-  ws.on('error', function incoming(event) {
-    console.log('error:')
-    console.log(event);
+  ws.on('error', (event) => {
+    console.error(`WSClient: error ${error}`)
   });
 
-  ws.on('close', function incoming(event) {
-    console.log('close:')
-    console.log(event);
+  ws.on('close', (code, reason) => {
+    console.log(`WSClient: close, code: ${code}, reason: ${reason}`)
   });
 }
 
@@ -243,64 +241,22 @@ const getExchangeInfo = (testnet = true) => {
     })
   })
 }
-/*
-const getTheXMostTradedPairsByVolumeOld = async (testnet = true, max = 10, comparisonBaseSymbol = 'USDT') => {
-  const res = await get24HStats();
 
-  // list of heuristics:
-  // - USD === USDT:)
-
-  const pairsToSort = [];
+const getTheXMostTradedPairsByRelativeVolume = async (testnet = true, max = 10) => {
   const output = [];
-  const usdtPriceMap = {}
-  const tempPairs = [];
-  res.forEach((element) => {
-    const currentPair = {
-      symbol: element.symbol,
-      weightedAvgPrice: element.weightedAvgPrice,
-      volume: element.volume,
-      quoteVolume: element.quoteVolume
-    }
-    if (element.symbol.indexOf('USDT') > 0) {
-      pairsToSort.push({
-        ...currentPair,
-        quoteVolumeUSDT: element.quoteVolume,
-        quoteVolumeUSDTNum: Number(element.quoteVolume)
-      })
-      usdtPriceMap[element.symbol.replace('USDT', '')] = Number(element.weightedAvgPrice)
-    }
-    else {
-      tempPairs.push(currentPair)
-    }
-  })
+  const stats24 = await get24HStats(false);
+  stats24.sort((a, b) => Number(b.volume) - Number(a.volume))
 
-  tempPairs.forEach((element) => {
-    const symbol = element.symbol.replace('USDT', '')
-    // todo
-    const assetPriceInUSDT = usdtPriceMap[symbol];
-    if (assetPriceInUSDT) {
-      const usdtPrice = Number(element.volume) * assetPriceInUSDT;
-      pairsToSort.push({
-        symbol: element.symbol,
-        weightedAvgPrice: element.weightedAvgPrice,
-        volume: element.volume,
-        quoteVolumeUSDT: `${usdtPrice}`,
-        quoteVolumeUSDTNum: usdtPrice
-      })
-    }
-  })
-
-  pairsToSort.sort((a, b) => b.quoteVolumeUSDTNum - a.quoteVolumeUSDTNum)
   for (let i = 0; i < max; i++) {
-    if (pairsToSort[i]) {
-      output.push(pairsToSort[i])
+    if (stats24[i]) {
+      output.push(stats24[i])
     }
   }
-  console.log(output.length, output)
-}
-*/
 
-const getTheXMostTradedPairsByVolume = async (testnet = true, max = 10, comparisonBaseSymbol = 'USDT') => {
+  return output;
+}
+
+const getTheXMostTradedPairsByVolumeInSymbol = async (testnet = true, max = 10, comparisonBaseSymbol = 'USDT') => {
   const stats24 = await get24HStats(false);
   const pairsToSort = [];
   const statsMap = {}
@@ -315,16 +271,16 @@ const getTheXMostTradedPairsByVolume = async (testnet = true, max = 10, comparis
     }
     statsMap[element.symbol] = currentPair
 
-    if (element.symbol.indexOf('USDT') > 0) {
+    if (element.symbol.indexOf(comparisonBaseSymbol) > 0) {
       currentPair = {
         ...currentPair,
-        quoteVolumeUSDT: element.quoteVolume,
-        quoteVolumeUSDTNum: Number(element.quoteVolume)
+        quoteVolumeBase: element.quoteVolume,
+        quoteVolumeBaseNum: Number(element.quoteVolume)
       }
       statsMap[element.symbol] = currentPair
 
       pairsToSort.push(currentPair)
-      usdtPriceMap[element.symbol.replace('USDT', '')] = Number(element.weightedAvgPrice)
+      usdtPriceMap[element.symbol.replace(comparisonBaseSymbol, '')] = Number(element.weightedAvgPrice)
     }
   })
 
@@ -333,49 +289,75 @@ const getTheXMostTradedPairsByVolume = async (testnet = true, max = 10, comparis
   exchangeInfo.symbols.forEach((el) => {
     const pairStats = statsMap[`${el.symbol}`]
     const assetPriceInUSDT = usdtPriceMap[`${el.baseAsset}`]
-    if (pairStats && !pairStats.quoteVolumeUSDTNum && assetPriceInUSDT) {
+    if (pairStats && !pairStats.quoteVolumeBaseNum && assetPriceInUSDT) {
       const volumePrice = assetPriceInUSDT * Number(pairStats.volume);
       pairsToSort.push({
         ...pairStats,
-        quoteVolumeUSDT: `${volumePrice}`,
-        quoteVolumeUSDTNum: volumePrice
+        quoteVolumeBase: `${volumePrice}`,
+        quoteVolumeBaseNum: volumePrice
       })
     }
   })
 
   const output = [];
-  pairsToSort.sort((a, b) => b.quoteVolumeUSDTNum - a.quoteVolumeUSDTNum)
+  pairsToSort.sort((a, b) => b.quoteVolumeBaseNum - a.quoteVolumeBaseNum)
   for (let i = 0; i < max; i++) {
     if (pairsToSort[i]) {
       output.push(pairsToSort[i])
     }
   }
-  console.log('finish 3')
-  console.log(Date.now())
-  console.log(output.length, output)
+
+  return output;
 }
 
 
 
 (async () => {
-  // const state = {
-  //   spot: {
-  //     nonZeroBalances: []
-  //   }
-  // }
-  // const spotAccountInfo = await getSpotAccountInfo();
-  // // console.log('spotAccountInfo', spotAccountInfo)
-  // state.spot.nonZeroBalances = spotAccountInfo.balances.filter((element) => Number(element.free) > 0);
-  // console.log(`Current non-zero spot account balances: `, state.spot.nonZeroBalances)
-  //
-  //
-  // const listenKey = await getWSListenKey()
-  // periodicallyRefreshListenKey(listenKey)
-  // // open WS
-  // createWSClient(getWsUrl(listenKey), state)
-  // const res = await getTheXMostTradedPairsByVolume();
-  // console.log(res)
-  getTheXMostTradedPairsByVolume()
+  /*
+    Task: "Log to console current non 0 asset balances available on the SPOT account (testnet)"
+   */
+  const state = {
+    spot: {
+      nonZeroBalances: {}
+    }
+  }
+  const spotAccountInfo = await getSpotAccountInfo();
+  spotAccountInfo.balances
+    .filter((element) => Number(element.free) > 0 || Number(element.locked) > 0)
+    .forEach((element) => {
+      state.spot.nonZeroBalances[`${element.asset}`] = { free: element.free, locked: element.locked }
+    });
+
+  console.log(`Current non-zero spot account balances: `, state.spot.nonZeroBalances)
+
+  /*
+    Tasks:
+      - "Open a single userData websocket (with all the requirement logic to keep the listenKey active)"
+      - "Keep your local asset balances state up to date based on the data coming from userData"
+      - "Log the asset balances again on every balance change"
+   */
+  const listenKey = await getWSListenKey()
+  periodicallyRefreshListenKey(listenKey)
+  createWSClient(getWsUrl(listenKey), state)
+
+  /*
+    Tasks:
+      - TODO: "Open 10 *@trade websockets for the 10 pairs with the highest volume in the last 24h on the SPOT exchange (mainnet)"
+      - "Determinate the 10 pairs dynamically (no hard-coded pairs)"
+
+    The 10 pairs could be interpreted in multiple ways, the most logical is to assume the volume in some representative stablecoin/fiat volume,
+    if you would like to simply see the results by the relative volume of the pair please the call of getTheXMostTradedPairsByRelativeVolume() fn
+    anyway both of those algorithms have big O complexity time (n * log(n)) due to native Array.sort call which is basically a merge sort
+   */
+  // const most10TradedPairsStats = await getTheXMostTradedPairsByVolumeInSymbol() // or -> getTheXMostTradedPairsByRelativeVolume()
+  // const most10TradedPairs = most10TradedPairsStats.map((el) => el.symbol)
+  // console.log(most10TradedPairs)
+
+  /*
+    Task: TODO: "Measure event time => client receive time latency and log (min/mean/max) to console every 1 minute"
+   */
+
+  // https://binance-docs.github.io/apidocs/spot/en/#test-connectivity
 })()
 
 
